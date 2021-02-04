020364ef0cc3bde7a0baffa07ab048ac584e7f77
Tushar Nikam <champ96k@gmail.com>
2021-02-04 18:46:36 +0530

    52 ms

--- solutions/217. Contains Duplicate.cpp
+++ solutions/217. Contains Duplicate.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+    
+      // method 1: Using unordered_set
         
-        unordered_set<int>s;
-        for(int i=0;i<nums.size();i++) {
-            if(s.find(nums[i])!=s.end()) {
+      //   unordered_set<int>s;
+      //   for(int i=0;i<nums.size();i++) {
+      //       if(s.find(nums[i])!=s.end()) {
+      //           return true;
+      //       } else {
+      //           s.insert(nums[i]);
+      //       }
+      //   }
+      // return false;
+        
+      // Method 2: Using set
+        set<int>s;
+       for(int n:nums) {
+            if(s.find(n)==s.end())
+                s.insert(n);
+            else 
                 return true;
-            } else {
-                s.insert(nums[i]);
-            }
-        }
-      return false;
+        }
+        return false;
     }
 };
